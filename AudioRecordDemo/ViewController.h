bc5b0bc4c5507a149bf73884ba0ab3875f425539
Your Name <you@example.com>
2018-08-02 09:43:15 +0800

    Initial Commit

---
+++ AudioRecordDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioRecordDemo
+//
+//  Created by Cary on 2018/8/2.
+//  Copyright © 2018年 Cary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
